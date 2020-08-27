d2f01a7262529c4c1681041f8b2da357e4f35c44
Leon Jamison <leonjamison@icloud.com>
2020-08-27 18:21:20 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1> LETS BUILD NOMIFY-U!!! </h1>
+        <p> A single page application allowing a user to search movies and nominate 
+            their top 5 films for The Shoppies Entrepreneur Movie Awards. 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <span> By: Leon Jamison - Future Shopify Employee </span>
+      </div>
+    )
+  }
 }
-
-export default App;
